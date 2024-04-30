b3430d01f76157cf72078ed16162fdc793e3982c
40236370 <conall@live.co.uk>
2024-04-30 20:27:19 +0100

    UI Fixed - DataTables now display correctly Need to add Bar Chart to display data with stats. And a download button to download the data. Also, need to add the help section & admin.

--- app/static/dataTableInit.js
+++ app/static/dataTableInit.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
     // Initialize the DataTable with dummy data on page load
     window.table = $('#predictionsTable').DataTable({
-        "data": [{
+        "pagingType": "full_numbers", // Enables full pagination control
+        "lengthChange": true, // Allows the user to change the number of records per page
+        "searching": true, // Enables the search box
+        "order": [[3, "asc"]], // Sets the default ordering (in this case based on column 4)
+        "info": true, // Shows the 'Showing 1 to X of X entries' information
+        "autoWidth": false, // Disables automatic column width calculation
+        "data": [{          // Dummy data for initialization
             "Sample": "No Data Loaded",
             "Predicted Cluster": "No Data Loaded",
             "Highest Probability Score": "No Data Loaded"
         }],
-        "columns": [
+        "columns": [    // Defines the columns for the data table
             { "data": "Sample", "title": "Sample" },
             { "data": "Predicted Cluster", "title": "Predicted Cluster" },
             { "data": "Highest Probability Score", "title": "Highest Probability Score" }
